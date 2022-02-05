8df7eedbe4bedd2a871dea856f437b668a994e82
kulu.jian <kuluworker@gmail.com>
2022-02-06 03:02:08 +0800

    Add CourseWork_2

---
+++ src/main/java/pers/kulujian/coursework/coursework_2/PersonServiceImpl.java
@@ -0,0 +1,86 @@
+package pers.kulujian.coursework.coursework_2;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class PersonServiceImpl implements PersonService {
+	
+	@Autowired
+	private PersonDao personDao;
+	
+	@Override
+	public boolean append(String name, Date birth) {
+		Person person = new Person();
+		person.setName(name);
+		person.setAge(0);
+		person.setBirth(birth);
+		return append(person);
+	}
+	@Override
+	public boolean append(Person person) {
+		return personDao.create(person);
+	}
+
+	@Override
+	public List<Person> findAllPersons() {
+		return personDao.readAll();
+	}
+
+	@Override
+	public Person getPerson(String name) {
+		Optional<Person> optPerson = findAllPersons()
+				.stream()
+				.filter(p -> p.getName().equals(name))
+				.findFirst();
+		return optPerson.isPresent() ? optPerson.get() : null;
+	}
+
+	@Override
+	public List<Person> getPerson(Date birth) {
+
+		SimpleDateFormat sdf = new SimpleDateFormat("MMdd");
+		List<Person> person = findAllPersons()
+				.stream()
+				.filter(p -> sdf.format(p.getBirth()).equals(sdf.format(birth)))
+				.collect(Collectors.toList());
+		return person;
+	}
+
+	@Override
+	public List<Person> getPerson(int age) {
+		List<Person> person =
+		findAllPersons()
+		.stream()
+		.filter(p -> p.getAge() > age )
+		.collect(Collectors.toList());
+		return person;
+	}
+
+	@Override
+	public boolean modify(String name, Date birth) {
+		Person person = new Person();
+		person.setName(name);
+		person.setAge(0);
+		person.setBirth(birth);
+		return modify(person);
+	}
+	@Override
+	public boolean modify(Person person) {
+		return personDao.update(person);
+	}
+
+	@Override
+	public boolean remove(String name) {
+		Person person = new Person();
+		person.setName(name);
+		return personDao.delete(person);
+	}
+	
+}
---
+++ src/main/java/pers/kulujian/coursework/coursework_2/PersonSystem.java
@@ -0,0 +1,124 @@
+package pers.kulujian.coursework.coursework_2;
+
+import java.util.Scanner;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class PersonSystem {
+	private ApplicationContext ctx = new ClassPathXmlApplicationContext("applicationContext2.xml");
+	private PersonController personController = ctx.getBean("personController", PersonController.class);
+	private boolean stop;
+	
+	private void menu() {
+		System.out.println("=========================================");
+		System.out.println("1. 建立 Person 資料");
+		System.out.println("2. 取得 Person 全部資料");
+		System.out.println("3. 根據姓名取得 Person");
+		System.out.println("4. 取得今天生日的 Person");
+		System.out.println("5. 取得某一歲數以上的 Person");
+		System.out.println("6. 根據姓名來修改Person的生日");
+		System.out.println("7. 根據姓名來刪除Person");
+		System.out.println("0. 離開");
+		System.out.println("=========================================");
+		System.out.print("請選擇: ");
+		
+		Scanner sc = new Scanner(System.in);
+		int choice = sc.nextInt();
+		switch (choice) {
+			case 1:
+				createPerson();
+				break;
+			case 2:
+				printPersons();
+				break;	
+			case 3:
+				searchPersonByName();
+				break;
+			case 4:
+				searchBirchByToday();
+				break;
+			case 5:
+				searchPersonByAge();
+				break;
+			case 6:
+				updateBirthByName();
+				break;
+			case 7:
+				deletePersonByName();
+				break;
+			case 0:
+				System.out.println("離開系統");
+				stop = true;
+				break;
+		}
+	}
+	// 1. 建立 Person 資料
+	private void createPerson() {
+		System.out.print("請輸入姓名 生日年 月 日: ");
+		// Ex: Jack 1999 4 5
+		Scanner sc = new Scanner(System.in);
+		String name = sc.next();
+		int yyyy = sc.nextInt();
+		int mm = sc.nextInt();
+		int dd = sc.nextInt();
+		personController.addPerson(name, yyyy, mm, dd);
+	}
+	// 2. 取得 Person 全部資料
+	private void printPersons() {
+		personController.printAllPersons();
+	}
+	// 3. 根據姓名取得 Person
+	private void searchPersonByName() {
+		System.out.print("請輸入姓名: ");
+		// Ex: Jack
+		Scanner sc = new Scanner(System.in);
+		String name = sc.next();
+		personController.getPersonByName(name);
+	}
+	// 4. 取得今天生日的 Person
+	private void searchBirchByToday() {
+		personController.getPersonByBirth();
+	}
+	// 5. 取得某一歲數以上的 Person
+	private void searchPersonByAge() {
+		System.out.print("請輸入年齡: ");
+		// Ex: 18
+		Scanner sc = new Scanner(System.in);
+		int age = sc.nextInt();
+		personController.getPersonByAge(age);
+	}
+	// 6. 根據姓名來修改Person的生日
+	private void updateBirthByName() {
+		System.out.print("請輸入姓名及要修改的生日(yyyy mm dd)\n請使用空格分開： ");
+		// Ex: Jack 1999 1 1
+		Scanner sc = new Scanner(System.in);
+		String name = sc.next();
+		int yyyy = sc.nextInt();
+		int mm = sc.nextInt();
+		int dd = sc.nextInt();
+		personController.updatePerson(name, yyyy, mm, dd);
+		
+	}
+	// 7. 根據姓名來刪除Person
+	private void deletePersonByName() {
+		System.out.print("請輸入姓名: ");
+		// Ex: Jack
+		Scanner sc = new Scanner(System.in);
+		String name = sc.next();
+		personController.deletePerson(name);
+	}
+	
+	
+	public void start() {
+		while (!stop) {
+			menu();
+		}
+	}
+	
+	
+	public static void main(String[] args) {
+		new PersonSystem().start();
+	}
+
+}
