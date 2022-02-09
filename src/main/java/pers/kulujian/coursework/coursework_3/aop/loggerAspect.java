2499804b2a333d058acc7399942be288ff574a9e
kulu.jian <kuluworker@gmail.com>
2022-02-09 16:42:47 +0800

    Eclipse java編譯器異常，重設java版本，程式正常執行

--- src/main/java/pers/kulujian/coursework/coursework_3/aop/loggerAspect.java
+++ src/main/java/pers/kulujian/coursework/coursework_3/aop/loggerAspect.java
@@ -4,17 +4,20 @@ import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
+
+import pers.kulujian.coursework.coursework_3.template.EmpDao_homework;
 
 
 @Component	// 宣告接受管理
 @Aspect		// 宣告為 切面程式
-//@Order(1)	// 數字越小越先執行(預設是 int 的最大值，【21億多...】)
+@Order(1)	// 數字越小越先執行(預設是 int 的最大值，【21億多...】)
 public class loggerAspect {
 	
-//	@Autowired
-//	private EmpDao_homework empDao;
+	@Autowired
+	private EmpDao_homework empDao;
 
 	// PointCut 切入點，用來定義 Joinpoint 連接點 
 	@Pointcut(value = "execution(* pers.kulujian.coursework.coursework_3.template.EmpDao_homework.queryAll(..))")
@@ -30,15 +33,15 @@ public class loggerAspect {
 
 
 	// 前置通知：運行在目標方法執行之前，所以與例外發生沒有關係
-	@Before(value = "execution(* pers.kulujian.coursework.coursework_3.template.EmpDao_homework.queryAll(..))") // 軔入點表達式支援： &&、||、! ， 例如："pt() && !pt2()"
+	@Before(value = "pt1() || pt2()") // 軔入點表達式支援： &&、||、! ， 例如："pt() && !pt2()"
 	public void befor(JoinPoint joinPoint) {
 		// joinpoint
-//		String method_name = joinPoint.getSignature().getName().toString();
+		String method_name = joinPoint.getSignature().getName().toString();
 //		System.out.println(method_name);
-		System.out.println("測試看看，是否成功");
+//		System.out.println("測試看看，是否成功");
 		
-//		int rowcount = empDao.addLog(method_name);
-//			System.out.println("結果: " + (rowcount > 0 ? "紀錄完成" : "紀綠異常"));
+		int rowcount = empDao.addLog(method_name);
+			System.out.println("結果: " + (rowcount > 0 ? "紀錄完成" : "紀綠異常"));
 	}
 
 	// 返回通知：可以在切面程式中得到目標方法的回傳值
